3914d1d51340d5daba0261005372619a7b64a727
Arsh-Savaliya <arsh.savaliya63@gmail.com>
2025-11-16 20:53:50 +0530

    added taking ship coord and validation them

--- gameManager.py
+++ gameManager.py
@@ -8,6 +8,29 @@ class gameManager:
     def __init__(self,p1,p2):  # makes two player objects (handles extra spaces)
         self.player1 = player(input("Enter name of PLayer 1: ").strip())
         self.player2 = player(input("Enter name of player 2: ").strip())
+
+    def get_ship_coords(self,name,size):
+        while True:
+            start = tuple(map(int,input("Enter starting coords as 1 2 for (1,2): ").split()))
+            print(f"Keep in mind that ship size is {self.size}")
+            ending = tuple(map(int,input("Enter ending coords as 1 5 for (1.5): ").split()))
+            if (start[0]!=ending[0]) ^ (start[1]!=ending[1]):
+                 print("Keep in mind that ship can be either vertical or horizontal")
+                 continue
+                 
+            if (len(start) != 2) or (len(ending) != 2):
+                print("Please enter only two numbers")
+                continue
+            
+            if (start[0] == ending[0]):
+                 if (abs(start[1]-ending[0]) != size):
+                      print(f"Keep in mind that ship is of size {size}")
+                 coords = [(start[0],y) for y in range(start[1],ending[1]+1)]
+            elif (start[1]== ending[1]):
+                 coords = [(x,start[1]) for x in range(start[0],ending[0]+1)]
+            if not Board.validShipPlace(self.name,coords):
+                 continue
+        return coords
     
     def place_ships_randomly(self):
         print(f"\nPlacing ships randomly for {self.name}...")
